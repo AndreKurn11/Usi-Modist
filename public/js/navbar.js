32431d4c43e4c1da608dde004c74c2b7a493034f
AndreKurn11 <muhammadandre110311@gmail.com>
2026-08-19 16:01:42 +0700

    adalah itunya

---
+++ public/js/navbar.js
@@ -0,0 +1,27 @@
+const normalizePath = (path) => {
+  if (path.length > 1) {
+    return path.replace(/\/$/, "");
+  }
+
+  return path;
+};
+
+const currentPath = normalizePath(window.location.pathname);
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+  const linkPath = normalizePath(new URL(link.href).pathname);
+
+  if (linkPath === currentPath) {
+    link.classList.remove(
+      "text-stone-700",
+      "font-medium",
+      "border-transparent"
+    );
+
+    link.classList.add(
+      "text-stone-900",
+      "font-semibold",
+      "border-stone-800"
+    );
+  }
+});
